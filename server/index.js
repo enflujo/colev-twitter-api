d3af742b040983106ddc014d625203f60376f7f3
3st3r30sc0p10 <h.idarraga@uniandes.edu.co>
2022-02-27 00:09:00 -0500

    Actualizacion stream url

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
-const { MONGO_USER, MONGO_PASS } = process.env;
-const MONGODB_URI = `mongodb://${MONGO_USER}:${MONGO_PASS}@localhost:27017`;
+// const { MONGO_USER, MONGO_PASS } = process.env;
+// const MONGODB_URI = `mongodb://${MONGO_USER}:${MONGO_PASS}@localhost:27017`;
+const MONGODB_URI = process.env.MONGODB_URI;//Conexión local Hugo
 const mc = mongoose.connection;
 const entradaSchema = require('../models/Tweet');
 const http = require('http');
@@ -33,13 +34,14 @@ app.get('/prueba', (req, res) => {
 
 const rulesUrl = 'https://api.twitter.com/2/tweets/search/stream/rules';
 const streamUrl =
-  'https://api.twitter.com/2/tweets/search/stream?tweet.fields=id,text,created_at,referenced_tweets,conversation_id&expansions=author_id,referenced_tweets.id';
+  'https://api.twitter.com/2/tweets/search/stream?tweet.fields=attachments,author_id,context_annotations,created_at,entities,geo,id,in_reply_to_user_id,lang,possibly_sensitive,public_metrics,referenced_tweets,source,text,withheld&expansions=referenced_tweets.id,referenced_tweets.id.author_id,author_id,geo.place_id,attachments.media_keys,attachments.poll_ids,entities.mentions.username,in_reply_to_user&media.fields=duration_ms,height,media_key,preview_image_url,public_metrics,type,url,width,alt_text&user.fields=created_at,description,entities,id,location,name,pinned_tweet_id,profile_image_url,protected,url,username,verified,withheld,public_metrics&place.fields=contained_within,country,country_code,full_name,geo,id,name,place_type&poll.fields=duration_minutes,end_datetime,id,options,voting_status';
+  // 'https://api.twitter.com/2/tweets/search/stream?tweet.fields=id,text,created_at,referenced_tweets,conversation_id&expansions=author_id,author.verified,in_reply_to_user_id,referenced_tweets.id&user.fields=created_at,description,entities,id,location,name,pinned_tweet_id,profile_image_url,protected,url,username,verified,withheld&expansions=pinned_tweet_id';
 // ?tweet.fields=conversation_id,public_metrics,entities&expansions=author_id,in_reply_to_user_id,referenced_tweets.id,attachments.media_keys&user.fields=name,username,description&media.fields=preview_image_url,url';
 //curl --request GET 'https://api.twitter.com/2/tweets?ids=1136048014974423040&expansions=geo.place_id&place.fields=contained_within,country,country_code,full_name,geo,id,name,place_type'
 // const streamUrl =
 //   'https://api.twitter.com/2/tweets/search/stream?tweet.fields=text,created_at,referenced_tweets,attachments,geo,geo.coordinates,context_annotations,withheld,possibly_sensitive,lang,reply_settings,source,conversation_id,public_metrics,entities';
 
-const rules = [{ value: 'covid' }];
+const rules = [{ value: 'happy' }];
 
 //Get stream rules
 
@@ -98,12 +100,13 @@ function streamTweets() {
   stream.on('data', (data) => {
     try {
       const json = JSON.parse(data);
-      console.log(json);
       io.emit('tweet', json);
       collection.insertMany(
         {
           text: json.data.text,
-          author_username: json.data.author_username,
+          author_id: json.data.author_id,
+          id: json.data.id,
+          conversation_id: json.data.conversation_id
         },
         (err, result) => {
           if (err) console.log(err);
