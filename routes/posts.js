97b852451c22a15a6112276f6b530c23e214b5fb
Juan Camilo González <j.gonzalezj@uniandes.edu.co>
2022-03-15 09:26:45 -0500

    ⚡️ Implementar sistema de estilos

--- routes/posts.js
+++ routes/posts.js
@@ -2,64 +2,56 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../models/Post');
 
-
 router.get('/', async (req, res) => {
   try {
     const posts = await Post.find();
-    res.json(posts)
+    res.json(posts);
   } catch (err) {
-    res.json({message: err})
+    res.json({ message: err });
   }
-})
+});
 
 router.post('/', async (req, res) => {
   const post = new Post({
     date_time: req.body.date_time,
     confirmed: req.body.confirmed,
     death: req.body.death,
-    type: req.body.type
-  })
+    type: req.body.type,
+  });
   try {
     const savedPost = await post.save();
     res.json(savedPost);
+  } catch (err) {
+    res.json({ message: err });
   }
-  catch (err) {
-    res.json({message: err})
-  }
-})
+});
 
 //Specific
 router.get('/:postId', async (req, res) => {
   try {
     const post = await Post.findById(req.params.postId);
-  res.json(post)
+    res.json(post);
   } catch (err) {
-    res.json({message: err})
+    res.json({ message: err });
   }
-})
+});
 
 router.delete('/:postId', async (req, res) => {
   try {
     const removedPost = await Post.remove({ _id: req.params.postId });
-    res.json(removedPost)
+    res.json(removedPost);
   } catch (err) {
-    res.json({message: err})
+    res.json({ message: err });
   }
-})
- 
+});
+
 router.patch('/:postId', async (req, res) => {
   try {
-    const updatedPost = await Post.updateOne({ _id: req.params.postId },
-      { $set: { title: req.body.title } });
-    res.json(updatedPost)
+    const updatedPost = await Post.updateOne({ _id: req.params.postId }, { $set: { title: req.body.title } });
+    res.json(updatedPost);
   } catch (err) {
-    res.json({message: err})
+    res.json({ message: err });
   }
- })
+});
 
 module.exports = router;
-
-
-
-
-
