9d4c3c1e7c9f0303e50fb702f45723b5773b6396
3st3r30sc0p10 <estereoscopio@protonmail.com>
2022-03-21 17:42:24 -0500

    ultimos cambios por el stash para pasarme a una nueva rama

---
+++ utilidades/ayuda-visualizaciones.js
@@ -0,0 +1,6 @@
+export const calcularMeses = (inicio, final) => {
+  let meses = (final.getFullYear() - inicio.getFullYear()) * 12;
+  meses -= inicio.getMonth();
+  meses += final.getMonth();
+  return meses <= 0 ? 0 : meses;
+};
